f526c8d0c199cb6902b1f22f4986f33f0da2e48a
Gerardo Lima <glima@equalexperts.com>
2019-06-10 20:24:41 +0100

    add dependency 'enzyme'

---
+++ src/setupTests.js
@@ -0,0 +1,9 @@
+/**
+ * This file is used by 'react-scripts' to setup tests.
+ * @see https://facebook.github.io/create-react-app/docs/running-tests#initializing-test-environment
+ */
+
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
